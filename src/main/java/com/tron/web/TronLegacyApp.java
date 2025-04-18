3cd717c6892b6188372069b50b056be344a9c650
vivekvalderrama <126052209+vivekvalderrama@users.noreply.github.com>
2025-04-18 17:53:49 +0530

    TronLegacyApp.java

--- src/main/java/com/tron/web/TronLegacyApp.java
+++ src/main/java/com/tron/web/TronLegacyApp.java
@@ -2,9 +2,18 @@ package com.tron.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TronLegacyApp {
+public class TronLegacyApp extends SpringBootServletInitializer {
+
+    // This method is needed to configure the application for WAR deployment
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TronLegacyApp.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TronLegacyApp.class, args);
     }
